63b55569f0b736907bf19d3bc8d5cb9a22352484
evst <evst@tpu.ru>
2018-11-22 18:49:16 +0700

    Initial commit.

---
+++ src/main/java/com/pes/gcdclient/GcdClientApplication.java
@@ -0,0 +1,12 @@
+package com.pes.gcdclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcdClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcdClientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pes/gcdclient/GcdClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pes.gcdclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GcdClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
